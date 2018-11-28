a0cacef474cf9177a5bda1046a87bf47a8b00a0d
pulakanamnikitha <42571494+pulakanamnikitha@users.noreply.github.com>
2018-11-28 12:33:32 +0530

    Update and rename 55.py to 552.py

--- 55.py
+++
@@ -1,6 +0,0 @@
-n,m = map(int,raw_input().split())
-s=n*m
-if (n % 2) == 0:
-    print ("even")
-else:
-    print ("odd")
---
+++ 552.py
@@ -0,0 +1,6 @@
+p,o= map(int,raw_input().split())
+s=p*o
+if (p % 2) == 0:
+    print ("even")
+else:
+    print ("odd")
